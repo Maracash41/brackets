9c4c8a72c3e98e26a9c7140f6f88ae255cf72850
Maracash41 <9531945@gmail.com>
2023-10-23 16:10:23 +0300

    feat: Task Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const OPEN = Array.from(bracketsConfig.slice().map(item => item.slice()[0]));
+    const CLOSED = Object.fromEntries(bracketsConfig.slice().map(item => item.slice().reverse()));
+      let stack = [];
+      for (let i = 0; i < str.length; i++) {
+          let cur = str[i];
+        if (OPEN.includes(cur) != (Object.keys(CLOSED).includes(cur))) {
+          if (OPEN.includes(cur)) {
+            stack.push(cur);
+         }
+         else {
+           if(stack.length == 0) {
+            return false;
+           }
+             let top = stack[stack.length-1];
+             if(CLOSED[cur] == top) {
+                 stack.pop();
+             }
+             else {
+                 return false;
+             }
+         }
+        }
+        else {
+          if (OPEN.includes(cur) && Object.keys(CLOSED).includes(cur)) {
+            let top = stack[stack.length -1];
+            if (CLOSED[cur] == top) {
+              stack.pop();
+            }
+            else {
+              stack.push(cur);
+            }
+          }
+        }
+      }
+      if (stack.length == 0) {
+        return true;
+      } else {
+        return false;
+      }
 }
